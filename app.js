92dfd7f5f84b2171e885b308f70a07c1367bbd87
Sanjay Kumar Konakandla <sanjaykumarkonakandla@gmail.com>
2022-12-27 16:24:26 +0530

    Initial Commit with Ohms Law Logic

---
+++ app.js
@@ -0,0 +1,58 @@
+//jshint esversion:6
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static("public"));
+
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get("/OhmsLaw.html", function (req, res) {
+    res.sendFile(__dirname + '/OhmsLaw.html');
+});
+
+app.get("/OhmVoltage.html", function (req, res) {
+    res.sendFile(__dirname + '/OhmVoltage.html')
+})
+
+app.get("/OhmCurrent.html", function (req, res) {
+    res.sendFile(__dirname + '/OhmCurrent.html')
+})
+
+app.get("/OhmResistance.html", function (req, res) {
+    res.sendFile(__dirname + '/OhmResistance.html')
+})
+
+
+
+app.post("/OhmVoltage.html", function (req, res) {
+    // console.log(req.body);
+    var I = req.body['current'];
+    var R = req.body['resistance'];
+    var V = I * R;
+    res.send("Voltage is  " + V);
+})
+
+app.post("/OhmCurrent.html", function (req, res) {
+    // console.log(req.body);
+    var V = req.body['voltage'];
+    var R = req.body['resistance'];
+    var I = V / R;
+    res.send("Current is  " + I);
+})
+
+app.post("/OhmResistance.html", function (req, res) {
+    console.log(req.body);
+    var V = req.body['voltage'];
+    var I = req.body['current'];
+    var R = V / I;
+    res.send("Resistance is  " + R);
+})
+
+
+app.listen(3000, function () {
+    console.log("App is running on port 3000")
+});
