4c10d054dcd1a2e622d0f78068f11bfd8b54dcd4
Sanjay Kumar <94333583+sanjay-72@users.noreply.github.com>
2023-01-13 05:51:04 +0530

    PORT UPDATE to 80

--- app.js
+++ app.js
@@ -297,6 +297,6 @@ app.post("/WireThickness.html", function (req, res) {
 
 
 
-app.listen(3000, function () {
-    console.log("App is running on port 3000")
+app.listen(80, function () {
+    console.log("App is running on port 80")
 });
