444f80b7e68bbecb0b8b7e75d164019feba5b0ce
Sanjay Kumar Konakandla <sanjaykumarkonakandla@gmail.com>
2023-01-14 11:57:03 +0530

    Bug fixes and Style Updates

--- app.js
+++ app.js
@@ -13,9 +13,7 @@ app.get("/", function (req, res) {
     // res.sendFile(__dirname + '/index.html');
     myNum = Math.random() * 3;
     myNum = Math.round(myNum);
-    console.log(myNum);
     userCount = userCount + myNum;
-
     res.render("index", {
         userCount: userCount
     })
